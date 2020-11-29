f596b0eac2d9c1931b77fab911a454906ad68cd9
nickname038 <yulia032022@gmail.com>
2020-11-29 16:09:02 +0200

    create basic structure for organizations

---
+++ src/organizations/organizations.controller.js
@@ -0,0 +1,5 @@
+'use strict';
+
+function OrganizationsController() {}
+
+module.exports = OrganizationsController;
---
+++ src/organizations/organizations.dto.js
@@ -0,0 +1,5 @@
+'use strict';
+
+const OrganizationsDto = {};
+
+module.exports = { ...OrganizationsDto };
