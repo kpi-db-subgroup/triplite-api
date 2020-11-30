da0d499c3d0cd89a00fee8c8e8178c038f8277a1
nickname038 <yulia032022@gmail.com>
2020-11-30 23:30:07 +0200

    organizations module added and basic functions added to OrganizationsRepository

--- src/organizations/organizations.service.js
+++ src/organizations/organizations.service.js
@@ -1,5 +1,8 @@
 'use strict';
 
+const Injection = require('../core/injection');
+
+Injection.annotate(OrganizationsService, { injections: ['organizationsRepository'] });
 function OrganizationsService() {}
 
 module.exports = OrganizationsService;
