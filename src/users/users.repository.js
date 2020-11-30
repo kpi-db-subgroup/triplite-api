ecb58503cc3407fb2d40144c5c67e7faca49e249
Yehor Bublyk <yehor.bk@gmail.com>
2020-11-30 11:56:31 +0200

    dependency injection added in controller, service and repository

--- src/users/users.repository.js
+++ src/users/users.repository.js
@@ -1,5 +1,8 @@
 'use strict';
 
+const Injection = require('../core/injection');
+
+Injection.annotate(UsersRepository, { injections: ['database'] });
 function UsersRepository() {}
 
 module.exports = UsersRepository;
--- src/users/users.service.js
+++ src/users/users.service.js
@@ -1,5 +1,8 @@
 'use strict';
 
+const Injection = require('../core/injection');
+
+Injection.annotate(UsersService, { injections: ['usersRepository'] });
 function UsersService() {}
 
 module.exports = UsersService;
