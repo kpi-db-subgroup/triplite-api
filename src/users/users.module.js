fadbd559afabec2683cfa375bdb3ad5ead661c9d
Yehor Bublyk <yehor.bk@gmail.com>
2020-11-30 11:28:49 +0200

    users module added

---
+++ src/users/users.module.js
@@ -0,0 +1,11 @@
+'use strict';
+
+const UsersRepository = require('./users.repository');
+const UsersService = require('./users.service');
+const UsersController = require('./users.controller');
+
+module.exports = {
+  UsersRepository,
+  UsersService,
+  UsersController,
+};
