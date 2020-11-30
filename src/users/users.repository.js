3289c336ff5eaee478b16d245d728d68b09cab17
Yehor Bublyk <yehor.bk@gmail.com>
2020-11-30 14:02:08 +0200

    basic functions added to UsersRepository

--- src/users/users.repository.js
+++ src/users/users.repository.js
@@ -5,4 +5,24 @@ const Injection = require('../core/injection');
 Injection.annotate(UsersRepository, { injections: ['database'] });
 function UsersRepository() {}
 
+UsersRepository.prototype.create = function (user) {
+  return user; // TODO
+};
+
+UsersRepository.prototype.findById = function (id) {
+  return id; // TODO
+};
+
+UsersRepository.prototype.findAll = function () {
+  return null; // TODO
+};
+
+UsersRepository.prototype.update = function (id, user) {
+  return { id, user }; // TODO
+};
+
+UsersRepository.prototype.delete = function (id) {
+  return id; // TODO
+};
+
 module.exports = UsersRepository;
