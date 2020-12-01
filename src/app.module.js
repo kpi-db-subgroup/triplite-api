a7172de0a324a0cf845d4487e7878a690f88ab91
Yehor Bublyk <yehor.bk@gmail.com>
2020-12-01 14:23:18 +0200

    bug with creation of new instances for dependency injection fixed

--- src/app.module.js
+++ src/app.module.js
@@ -3,18 +3,24 @@
 const { lowerCaseFirstLetter } = require('./core/common');
 
 const UsersModule = require('./users/users.module');
+const OrganizationsModule = require('./organizations/organizations.module');
 
 const database = require('./database/database-interface');
 
 const extractInstances = obj =>
   Object.keys(obj).reduce((accumulator, current) => {
-    const Bean = obj[current];
-    accumulator[lowerCaseFirstLetter(current)] =
-      typeof bean === 'function' ? new Bean() : Bean;
+    let Bean = obj[current];
+    try {
+      Bean = new obj[current]();
+    } catch (exception) {
+      // TODO
+    }
+    accumulator[lowerCaseFirstLetter(current)] = Bean;
     return accumulator;
   }, {});
 
 module.exports = {
   ...extractInstances(UsersModule),
+  ...extractInstances(OrganizationsModule),
   database,
 };
