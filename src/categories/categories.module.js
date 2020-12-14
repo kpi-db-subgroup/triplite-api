ade8126625bea63bda7999a3941f64ec49679e9d
nickname038 <yulia032022@gmail.com>
2020-12-14 19:19:36 +0200

    categories module added and basic functions added

---
+++ src/categories/categories.module.js
@@ -0,0 +1,13 @@
+'use strict';
+
+const CategoriesEntityBuilder = require('./categories.entity-builder');
+const CategoriesRepository = require('./categories.repository');
+const CategoriesService = require('./categories.service');
+const CategoriesController = require('./categories.controller');
+
+module.exports = {
+  CategoriesEntityBuilder,
+  CategoriesRepository,
+  CategoriesService,
+  CategoriesController,
+};
--- src/categories/categories.repository.js
+++ src/categories/categories.repository.js
@@ -1,5 +1,41 @@
 'use strict';
 
+const Injection = require('../core/injection');
+
+Injection.annotate(CategoriesRepository, {
+  injections: ['database, categoriesEntityBuilder'],
+});
+
 function CategoriesRepository() {}
+
+CategoriesRepository.prototype.create = async function (category) {
+  const result = await this.database.run(`
+    INSERT INTO categories(name)
+    VALUES(${category.name})
+  `);
+  return result;
+};
+
+CategoriesRepository.prototype.findById = async function (id) {
+  const row = await this.database.get(`SELECT * FROM categories WHERE id=${id}`);
+  return this.categoriesEntityBuilder.getEntityFromRow(row);
+};
+
+CategoriesRepository.prototype.findAll = async function () {
+  const rows = await this.database.all('SELECT * FROM categories');
+  return this.categoriesEntityBuilder.getEntitiesFromRows(rows);
+};
+
+CategoriesRepository.prototype.update = async function (id, category) {
+  const result = await this.database.run(`
+    UPDATE categories SET name=${category.name}, WHERE id=${id}
+  `);
+  return result;
+};
+
+CategoriesRepository.prototype.delete = async function (id) {
+  const result = await this.database.run(`DELETE FROM categories WHERE id=${id}`);
+  return result;
+};
 
 module.exports = CategoriesRepository;
