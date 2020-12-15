9484337ffaea0cc73b9873c802bac3b275655094
Yehor Bublyk <35728917+yehorbk@users.noreply.github.com>
2020-12-15 10:12:54 +0200

    Update comments.service.js

--- src/comments/comments.service.js
+++ src/comments/comments.service.js
@@ -3,7 +3,6 @@
 const Injection = require('../core/injection');
 
 Injection.annotate(CommentsService, { injection: ['commentsRepository'] });
-
 function CommentsService() {}
 
 module.exports = CommentsService;
