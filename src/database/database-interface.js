6462fb76f58e750c9bd034192f915c07a3e6987f
Yehor Bublyk <yehor.bk@gmail.com>
2020-12-15 15:49:24 +0200

    environment file added, database-interface functionality updated

--- src/database/database-interface.js
+++ src/database/database-interface.js
@@ -2,12 +2,23 @@
 
 const sqlite3 = require('sqlite3').verbose();
 const { promisify } = require('util');
+const environment = require('../environment');
 
-const database = new sqlite3.Database(':memory:');
+const databasePath = environment.development
+  ? ':memory:'
+  : './db/triplite.db';
+
+const database = new sqlite3.Database(databasePath);
 
 const promisifiedInterface = {};
 promisifiedInterface.all = promisify(database.all.bind(database));
 promisifiedInterface.get = promisify(database.get.bind(database));
 promisifiedInterface.run = promisify(database.run.bind(database));
+
+promisifiedInterface.multipleQueriesRun = input => input
+  .replace(/(\r\n|\n|\r)/gm, '')
+  .split(';')
+  .filter(entry => entry.length > 0)
+  .forEach(entry => promisifiedInterface.run(entry));
 
 module.exports = promisifiedInterface;
