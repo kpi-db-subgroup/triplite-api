e5a4a723b3f5bb43e35e8f7d9f8096952f5edcec
Yehor Bublyk <yehor.bk@gmail.com>
2020-12-15 15:51:44 +0200

    typo fixed

--- src/database/database-interface.js
+++ src/database/database-interface.js
@@ -4,9 +4,7 @@ const sqlite3 = require('sqlite3').verbose();
 const { promisify } = require('util');
 const environment = require('../environment');
 
-const databasePath = environment.development
-  ? ':memory:'
-  : './db/triplite.db';
+const databasePath = environment.development ? ':memory:' : './db/triplite.db';
 
 const database = new sqlite3.Database(databasePath);
 
@@ -15,10 +13,11 @@ promisifiedInterface.all = promisify(database.all.bind(database));
 promisifiedInterface.get = promisify(database.get.bind(database));
 promisifiedInterface.run = promisify(database.run.bind(database));
 
-promisifiedInterface.multipleQueriesRun = input => input
-  .replace(/(\r\n|\n|\r)/gm, '')
-  .split(';')
-  .filter(entry => entry.length > 0)
-  .forEach(entry => promisifiedInterface.run(entry));
+promisifiedInterface.multipleQueriesRun = input =>
+  input
+    .replace(/(\r\n|\n|\r)/gm, '')
+    .split(';')
+    .filter(entry => entry.length > 0)
+    .forEach(entry => promisifiedInterface.run(entry));
 
 module.exports = promisifiedInterface;
