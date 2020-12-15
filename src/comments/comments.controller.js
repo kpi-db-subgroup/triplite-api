d1294423c3662733a186c6fbb9d7ada71d8e4fc9
Yehor Bublyk <35728917+yehorbk@users.noreply.github.com>
2020-12-15 10:12:33 +0200

    Update comments.controller.js

--- src/comments/comments.controller.js
+++ src/comments/comments.controller.js
@@ -3,7 +3,6 @@
 const Injection = require('../core/injection');
 
 Injection.annotate(CommentsController, { injections: ['commentsService'] });
-
 function CommentsController() {}
 
 module.exports = CommentsController;
